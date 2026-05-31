6061ca97765ed1b7c0bef7a53f062fbff94a59ef
ConfucianConfusion <12412625@mail.sustech.edu.cn>
2026-05-31 09:08:32 +0800

    Fix GitHub Pages white screen (set base path)

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 这一行是为了修复 GitHub Pages 上白屏和路径 404 的问题
   plugins: [react()],
   server: {
     fs: {
